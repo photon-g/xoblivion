96025d42e059f936532fd3b9c1a1f738d7e0ee2b
Robert King <robert.king@lockeandpartners.com>
2024-08-27 21:20:31 +0200

    X-4: dev-add context

--- .github/rc/context.sh
+++ .github/rc/context.sh
@@ -1,3 +1,27 @@
 #! /bin/bash
 
+sudo apt update
+sudo apt install nodejs -y
+sudo apt install npm -y
+npm init -y
+npm i -g npm@latest
+npm i npm-install-all
+npm i -g tailwindcss
+npm i -g postcss
+npm i -g autoprefixer
+npm i -g vue-i18n
+npm i -g vite@latest
+npm i -g @headlessui/vue
+npm i -g @heroicons/vue
+npm i -g @quasar/cli@latest
+npm i -g @quasar/extras@latest
+npm i -g axios@latest
+npm i -g pinia@latest
+npm i -g vue@3.0.0
+npm i -g vue-router@latest
+npm i -g vue3-cookies@latest
+npm i -g dotenv@latest
+npm i -g jwt-decode@latest
+npm i -g apexcharts@latest
+npm i -g vue3-apexcharts@latest
 echo "setting up node context ...."
